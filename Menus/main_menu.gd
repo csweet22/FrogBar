50420f5abd6a29094d12c49b0650f69f52b6204c
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2024-11-03 11:20:02 -0500

    polish

--- Menus/main_menu.gd
+++ Menus/main_menu.gd
@@ -1,4 +1,7 @@
 extends Control
 
+func _ready() -> void:
+	$VBoxContainer/StartButton.grab_focus()
+
 func _on_start_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://main_level.tscn")
