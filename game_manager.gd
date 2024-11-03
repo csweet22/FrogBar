3e3c9b1c5418ce664b37b5474d519eaefb46a9b4
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2024-11-03 11:26:44 -0500

    Yes

--- game_manager.gd
+++ game_manager.gd
@@ -5,13 +5,12 @@ var score: float = 0.0
 @export var game_timer: Timer
 
 # List of all frogs in the scene
-var frogs
+var frogs: Array[Node]
 
 signal game_ended
 signal score_changed(new_score: float)
 
 func _ready() -> void:
-	# load in all frogs in scene into list
 	game_timer.timeout.connect(load_end_menu)
 
 func remove_score(value: float) -> void:
@@ -28,6 +27,15 @@ func set_score(value: float) -> void:
 func get_score() -> float:
 	return score
 
+func start_game():
+	# load in all frogs in scene into list
+	frogs = find_nodes_with_script(get_tree().root, load("res://Entities/frog.gd"))
+	for frog in frogs:
+		print(frog.name)
+
+func start_timer():
+	game_timer.start()
+
 func on_game_end() -> void:
 	game_ended.emit()
 	load_end_menu()
@@ -42,3 +50,14 @@ func make_disturbance() -> void:
 func make_order_request() -> void:
 	# Loop through all frogs, find one that is either neutral or has a drink 
 	pass
+
+func find_nodes_with_script(root: Node, script: Script) -> Array[Node]:
+	var nodes_with_script: Array[Node] = []
+	
+	if root.get_script() == script:
+		nodes_with_script.append(root)
+	
+	for child in root.get_children():
+		nodes_with_script += find_nodes_with_script(child, script)
+	
+	return nodes_with_script
