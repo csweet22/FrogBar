935203ba09b6a36ab19d2f1e2c3adf192586d4e3
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2024-11-08 16:53:45 -0500

    maybe fixed

--- Menus/hud_menu.gd
+++ Menus/hud_menu.gd
@@ -3,8 +3,9 @@ extends Control
 @export var score_label: Label
 
 var pointer: int = 0
-@export var drinks: Array[Control]
-var free_spots: Array[int] = [0, 1, 2, 3, 4]
+
+@export var drink_order_scene: PackedScene
+var drink_orders: Array[Control]
 
 var display_score: float = 0.0:
 	get:
@@ -22,28 +23,27 @@ func _process(delta: float) -> void:
 	$Main/VBoxContainer/TimeRemaining.text = "Time Remaining: " + "%02d:%02d" % [GameManager.game_timer.time_left / 60, fmod(GameManager.game_timer.time_left, 60)]
 
 func add_drink(type: Drinks.DrinkType):
-	var index = free_spots.pick_random()
-	free_spots.erase(index)
+	var instance = drink_order_scene.instantiate()
 	
 	match type:
 		Drinks.DrinkType.A:
-			drinks[index].activate(preload("res://Gfx/Martini.png"), Drinks.DrinkType.A)
+			instance.activate(preload("res://Gfx/Martini.png"), Drinks.DrinkType.A)
 		Drinks.DrinkType.B:
-			drinks[index].activate(preload("res://Gfx/Rum&Croak_center.png"), Drinks.DrinkType.B)
+			instance.activate(preload("res://Gfx/Rum&Croak_center.png"), Drinks.DrinkType.B)
 		Drinks.DrinkType.C:
-			drinks[index].activate(preload("res://Gfx/SwampWater.png"), Drinks.DrinkType.C)
+			instance.activate(preload("res://Gfx/SwampWater.png"), Drinks.DrinkType.C)
+	drink_orders.append(instance)
+	$Main/VBoxContainer/TaskContainer/HBoxContainer.add_child(instance)
 
 func remove_drink(type: Drinks.DrinkType):
 	var found_drink_to_remove = false
 	
-	var index = 0;
-	for drink in drinks:
+	for drink in drink_orders:
 		if drink.drink_type == type:
 			drink.deactivate()
-			free_spots.append(index)
+			drink_orders.erase(drink)
 			found_drink_to_remove = true
 			break
-		index += 1
 	
 	if not found_drink_to_remove:
 		print("ISSUE AROSE: COULD NOT REMOVE DRINK")
--- drink_order.gd
+++ drink_order.gd
@@ -3,12 +3,8 @@ extends MarginContainer
 var drink_type: Drinks.DrinkType
 
 func activate(texture: Texture2D, type: Drinks.DrinkType):
-	print("Activated.")
 	drink_type = type
 	$DrinkImage.texture = texture
-	visible = true
 
 func deactivate():
-	print("Deactivated.")
-	visible = false
-	
+	queue_free()
